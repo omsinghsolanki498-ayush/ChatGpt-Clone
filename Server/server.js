faf9226e54c5669bffd2af83e0efd786d92023c6
omsinghsolanki498@gmail.com <omsinghsolanki498@gmail.com>
2026-06-02 18:48:08 +0530

    fix: cors updated with new vercel url

--- Server/server.js
+++ Server/server.js
@@ -17,7 +17,8 @@ mongoose.connect(process.env.MONGO_URI)  // mongoose connection for .env URI
 
 app.use(cors({
   origin: [
-    "https://chat-gpt-clone-one-delta.vercel.app"
+    "http://localhost:5173",
+     "https://chat-gpt-clone-seven-blue.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
